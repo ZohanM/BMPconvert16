f4995f2d22a36ed1dc093838dfe5336861307b67
ZohanM <55000575+ZohanM@users.noreply.github.com>
2020-04-16 16:19:08 -0500

    Add files via upload

---
+++ images/cmake-build-debug/CMakeFiles/images.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/images.dir/main.c.obj"
+  "images.exe"
+  "images.exe.manifest"
+  "images.pdb"
+  "libimages.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/images.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
